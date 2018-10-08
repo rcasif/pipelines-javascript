70c9b996ed134130483548246b78efee45bd05af
rcasif <43175745+rcasif@users.noreply.github.com>
2018-10-08 23:31:14 +0500

    Update deploy.sh

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -5,22 +5,22 @@
 pushd .
 cd ~
 curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
-bash nodesource_setup.sh
-apt-get install nodejs -y
-apt-get install build-essential -y
+sudo bash nodesource_setup.sh
+sudo apt-get install nodejs -y
+sudo apt-get install build-essential -y
 
 # install express
-npm install express -y -g
+sudo npm install express -y -g
 popd
 
 # configure nginx
-rm -f /etc/nginx/sites-enabled/default
-cp -f ./nginx-config /etc/nginx/sites-available/example
-ln -fs /etc/nginx/sites-available/example /etc/nginx/sites-enabled
-service nginx reload
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo cp -f ./nginx-config /etc/nginx/sites-available/example
+sudo ln -fs /etc/nginx/sites-available/example /etc/nginx/sites-enabled
+sudo service nginx reload
 
 # install pm2 and start the node server as a daemon
 cd ..
-npm install pm2 -g -y
+sudo npm install pm2 -g -y
 pm2 delete example -s
 pm2 start server.js -n example
